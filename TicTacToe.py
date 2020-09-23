e53e478148d8eb6f3e1a0b1b7d8bc3905c2aa31d
PrerakGada <prerak.gada07@gmail.com>
2020-09-23 22:35:33 +0530

    update

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,67 +8,73 @@ def displayboard():
     print(board[3] + "|" + board[4] + "|" + board[5])
     print("-+-+-")
     print(board[6] + "|"+  board[7] + "|" + board[8])
-    print("\n\n")
+    print("\n")
 
 def move():
-    pass
+    displayboard()
+
+    pos = int(input("Enter the location you want to play: "))
+    if board[pos] == " ":
+        board[pos-1] = turn
+        MoveCount += 1
+        WinLose()
+    else:
+        print("This place is already filled") 
+        move(turn)
 
 def WinLose():
     if board[0] == board[1] == board[2] != " ":
         if board[0] == "X":
             print("X wins")
-        else:
+        elif board[0] == "O":
             print("O Wins")
 
     elif board[3] == board[4] == board[5] != " ":
-        if board[0] == "X":
+        if board[3] == "X":
             print("X wins")
-        else:
+        elif board[3] == "O":
             print("O Wins")
 
     elif board[6] == board[7] == board[8] != " ":
-        if board[0] == "X":
+        if board[6] == "X":
             print("X wins")
-        else:
+        elif board[6] == "O":
             print("O Wins")
 
     elif board[0] == board[3] == board[6] != " ":
         if board[0] == "X":
             print("X wins")
-        else:
+        elif board[0] == "O":
             print("O Wins")
 
     elif board[1] == board[4] == board[7] != " ":
-        if board[0] == "X":
+        if board[1] == "X":
             print("X wins")
-        else:
+        elif board[1] == "O":
             print("O Wins")
 
     elif board[2] == board[5] == board[8] != " ":
-        if board[0] == "X":
+        if board[2] == "X":
             print("X wins")
-        else:
+        elif board[2] == "O":
             print("O Wins")
     else:
-        move()
+        if " " in board:
+            player()
+        else:
+            print("The Game is Draw!!")
 
 
 # Control Code
-
-displayboard()
-
 MoveCount = 0
-for MoveCount in range(9):
+turn = "X"
+def player():
     if MoveCount % 2 == 0:
         print("It's X's Turn")
-        pos = int(input("Enter the location you want to play: "))
-        if board[pos] == " ":
-            board[pos-1] = "X"
-        else:
-            print("This place is already filled")
+        turn = "X"
     else:
         print("It's O's Turn")
-        pos = int(input("Enter the location you want to play: "))
-        board[pos-1] = "O"
-    displayboard()
-    WinLose()
+        turn = "O"
+    move()
+
+player()
