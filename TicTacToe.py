8c1b7499c2bde3e2df0eb11229270989a178bd05
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-09-25 00:30:05 +0530

    Under Development

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,70 +10,95 @@ def displayboard():
     print(board[6] + "|"+  board[7] + "|" + board[8])
     print("\n")
 
+def WinLose():
+    if board[0] == board[1] == board[2] != " ":
+        Game = False
+        if board[0] == "X":
+            print("X wins")
+        elif board[0] == "O":
+            print("O Wins")
+        else:
+            Game = True
+
+    elif board[3] == board[4] == board[5] != " ":
+        Game = False
+        if board[3] == "X":
+            print("X wins")
+        elif board[3] == "O":
+            print("O Wins")
+        else:
+            Game = True
+
+    elif board[6] == board[7] == board[8] != " ":
+        Game = False
+        if board[6] == "X":
+            print("X wins")
+        elif board[6] == "O":
+            print("O Wins")
+        else:
+            Game = True
+
+    elif board[0] == board[3] == board[6] != " ":
+        Game = False
+        if board[0] == "X":
+            print("X wins")
+        elif board[0] == "O":
+            print("O Wins")
+        else:
+            Game = True
+
+    elif board[1] == board[4] == board[7] != " ":
+        Game = False
+        if board[1] == "X":
+            print("X wins")
+        elif board[1] == "O":
+            print("O Wins")
+        else:
+            Game = True
+
+    elif board[2] == board[5] == board[8] != " ":
+        Game = False
+        if board[2] == "X":
+            print("X wins")
+        elif board[2] == "O":
+            print("O Wins")
+        else:
+            Game = True
+        
+    else:
+        if " " not in board:
+            print("The Game is Draw!!")
+            Game = False
+
+
 def move():
     displayboard()
 
     pos = int(input("Enter the location you want to play: "))
     if board[pos] == " ":
         board[pos-1] = turn
-        MoveCount += 1
         WinLose()
     else:
         print("This place is already filled") 
-        move(turn)
+        move()
 
-def WinLose():
-    if board[0] == board[1] == board[2] != " ":
-        if board[0] == "X":
-            print("X wins")
-        elif board[0] == "O":
-            print("O Wins")
-
-    elif board[3] == board[4] == board[5] != " ":
-        if board[3] == "X":
-            print("X wins")
-        elif board[3] == "O":
-            print("O Wins")
-
-    elif board[6] == board[7] == board[8] != " ":
-        if board[6] == "X":
-            print("X wins")
-        elif board[6] == "O":
-            print("O Wins")
-
-    elif board[0] == board[3] == board[6] != " ":
-        if board[0] == "X":
-            print("X wins")
-        elif board[0] == "O":
-            print("O Wins")
-
-    elif board[1] == board[4] == board[7] != " ":
-        if board[1] == "X":
-            print("X wins")
-        elif board[1] == "O":
-            print("O Wins")
-
-    elif board[2] == board[5] == board[8] != " ":
-        if board[2] == "X":
-            print("X wins")
-        elif board[2] == "O":
-            print("O Wins")
-    else:
-        if " " in board:
-            player()
-        else:
-            print("The Game is Draw!!")
-
-
-# Control Code
-MoveCount = 0
-turn = "X"
-def player():
+        
+def player(t):
     if MoveCount % 2 == 0:
         print("It's X's Turn")
-        turn = "X"
+        t = "X"
     else:
         print("It's O's Turn")
-        turn = "O"
+        t = "O"
+    return t
+
+# Control Code
+Game = True
+MoveCount = 0
+turn = "X"
+
+while Game:
+    turn = player(turn)
     move()
-player()
+    MoveCount += 1
+    WinLose()
